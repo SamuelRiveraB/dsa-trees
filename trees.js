558afd53cb09c3fde47aabfd645f8bbdeed77ae1
Samuel <samuelrivba@gmail.com>
2024-04-06 19:51:37 -0500

    Add traverse function to show the tree

--- trees.js
+++ trees.js
@@ -11,9 +11,26 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value) {}
+  insert(value) {
+    return this;
+  }
 
   lookup() {}
 }
 
 const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(9);
+tree.insert(9);
+tree.insert(9);
+tree.insert(9);
+tree.insert(9);
+tree.insert(9);
+JSON.stringify(traverse(tree.root));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
