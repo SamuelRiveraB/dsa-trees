bb05f4f9b23bf36a007f9250e05c7cccf0e98a3d
Samuel <samuelrivba@gmail.com>
2024-04-06 20:04:53 -0500

    Add insert method

--- trees.js
+++ trees.js
@@ -12,6 +12,29 @@ class BinarySearchTree {
   }
 
   insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
     return this;
   }
 
@@ -20,13 +43,13 @@ class BinarySearchTree {
 
 const tree = new BinarySearchTree();
 tree.insert(9);
-tree.insert(9);
-tree.insert(9);
-tree.insert(9);
-tree.insert(9);
-tree.insert(9);
-tree.insert(9);
-JSON.stringify(traverse(tree.root));
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)));
 
 function traverse(node) {
   const tree = { value: node.value };
